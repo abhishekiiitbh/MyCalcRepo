2752930d8417580336ef02efd7782095728f2a66
Abhishek Pandit <38048933+abhishekiiitbh@users.noreply.github.com>
2020-10-20 10:56:08 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -3,8 +3,12 @@ class Calc{
   public static int add(int a,int b){
     return a+b;
   }
+  public static int sub(int a,int b){
+    return a-b;
+  }
   
   public static void main(String []args){
   System.out.print(add(10,20));
+    System.out.print(sub(50,40));
   }
 }
